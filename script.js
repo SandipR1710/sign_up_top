af4cb1a5a4e92ec6dd66ad1b1e1c587aa24ab5cd
Sandip Roy <sandyishero72@gmail.com>
2023-07-01 12:47:11 +0530

    mobile version done and phone constraint added

--- script.js
+++ script.js
@@ -1,9 +1,15 @@
 function valid(){
     const pass=document.forms['form1']['pass'];
     const cpass=document.forms['form1']['cpass'];
+    const phone=document.forms['form1']['phone'];
     if(pass.value!==cpass.value){
-        cpass.parentNode.classList.add('wrong');
+        cpass.parentNode.classList.add('wrongmatch');
         return false;
     }
+    if(phone.value.length!=10){
+        phone.parentNode.classList.add('wrongphone');
+        return false;
+    }
+
     return true;
 }
